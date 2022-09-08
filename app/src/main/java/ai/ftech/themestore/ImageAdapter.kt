c65d202913f6f0756dcaecae74028f4f1d34e62c
Thom <thomnt01111111@gmail.com>
2022-09-08 13:38:36 +0700

    ThemeStore

--- app/src/main/java/ai/ftech/themestore/ImageAdapter.kt
+++ app/src/main/java/ai/ftech/themestore/ImageAdapter.kt
@@ -1,6 +1,7 @@
 package ai.ftech.themestore
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,42 +10,42 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class ImageAdapter(private var listImageUrls: MutableList<String>,
-                   private var listContext : Context)
-    : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>(){
+class ImageAdapter(
+    private var listImageUrls: MutableList<String>,
+    private var listContext: Context
+) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageAdapter.ImageViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.image_item, parent, false)
         return ImageViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ImageAdapter.ImageViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
         var image = listImageUrls.get(position)
-        if(image == null){
+        if (image == null) {
             return
         }
-//        holder.ivImage.setImageResource(image.RESOURCE_IMAGE_ID)
-//        holder.ibImage.setImageResource(image.IMAGE_BUTTON_SELECT)
+
         Glide.with(listContext)
             .load(listImageUrls[position])
-            .override(800,800)
+            .override(800, 800)
             .into(holder.ivImage)
     }
 
     override fun getItemCount(): Int {
-        if(listImageUrls != null){
+        if (listImageUrls != null) {
             return listImageUrls.size
-        }
-        else
+        } else
             return 0
     }
 
-    class ImageViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-        var ivImage : ImageView
-        var ibImage : ImageButton
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var ivImage: ImageView
+        private var ibImage: ImageButton
+
         init {
             ivImage = itemView.findViewById(R.id.ivImage)
-                 ibImage = itemView.findViewById(R.id.ibSelect)
+            ibImage = itemView.findViewById(R.id.ibSelect)
         }
     }
 }
